2ceea51bbdc61f13c82885c648c8bd2519baa8da
Ramya Krishnan <ramyaneela36@gmail.com>
2022-07-17 03:41:15 -0400

    contact page updated

--- function.js
+++ function.js
@@ -1,9 +1,14 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+const hamburger = document.getElementById('hamburger');
+const topNav = document.getElementById('topnav');
+
+hamburger.addEventListener('click',() => {
+  topNav.classList.toggle('show');
+});
+
+function validateForm() {
+  let x = document.forms["myForm"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
   }
-  
+}
